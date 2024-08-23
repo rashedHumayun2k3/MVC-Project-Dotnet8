55aed6697631dea6b9d6b5a70684c36d4e824cb1
Hrashed <Hrashed@nmef.com>
2024-08-23 15:57:22 +0600

    Add project files.

---
+++ MVC-Project-Dotnet8/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Project_Dotnet8.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
